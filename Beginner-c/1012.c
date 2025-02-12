af3527685cd50aacdcf2da1f649be9d5b86fbd49
LULc4s <lucasstavares.computerengineer@gmail.com>
2025-02-12 17:06:00 -0300

    Update 1012.c

--- Beginner-c/1012.c
+++ Beginner-c/1012.c
@@ -4,4 +4,4 @@ int main(){
     double a, b, c;
     scanf("%lf %lf %lf", &a, &b, &c);
     printf("TRIANGULO: %.3lf\n", (a * c) / 2);
-}aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+}
